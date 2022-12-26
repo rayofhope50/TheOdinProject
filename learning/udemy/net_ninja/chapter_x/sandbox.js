bd85a87dc494be6f607ea485ae2c1ee2f8484566
rayofhope50 <przemox2@gmail.com>
2022-12-26 17:57:31 +0000

    start java course udemy

--- learning/udemy/net_ninja/chapter_x/sandbox.js
+++ learning/udemy/net_ninja/chapter_x/sandbox.js
@@ -1,11 +1,16 @@
-let age = 25;
-let year = 2019;
+const grade = 'D';
 
-console.log(age, year);
-
-age = 30;
-console.log(age);
-
-const points = 100;
-
-console.log(points);
+switch (grade) {
+    case 'A':
+        console.log('U got an A!');
+    case 'B':
+        console.log('U got an B!');
+    case 'C':
+        console.log('U got an C!');
+    case 'D':
+        console.log('U got an D!');
+    case 'E':
+        console.log('U got an E!');
+    default:
+        console.log('Not a valid grade');
+}
