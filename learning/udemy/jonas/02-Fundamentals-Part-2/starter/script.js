ca6ce6aae5080b1f1b21e4bb783c0453244edd8a
rayofhope50 <przemox2@gmail.com>
2022-12-28 20:12:22 +0000

    start of fundamentals 2

---
+++ learning/udemy/jonas/02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,65 @@
+'use strict';
+
+// let hasDriversLicence = false;
+// const passTest = true;
+
+// if (passTest) hasDriversLicence = true;
+// if (hasDriversLicence) console.log('I can drive :D');
+// function logger() {
+//     console.log('My name is Jonas');
+// }
+// logger();
+// function fruitProcessor(apples, oranges) {
+//     const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//     return juice;
+// }
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
+
+// const appleOrangeJuice = fruitProcessor(2, 4);
+// console.log(appleOrangeJuice);
+
+// function describeCountry(country, population, capitalCity) {
+//     const description = `${country} has ${population} milion people and its capital city is ${capitalCity}.`
+//     return description;
+// }
+
+// const poland = describeCountry('Poland', 38, 'Warsaw');
+// console.log(poland);
+
+// const germany = describeCountry('Germany', 45, 'Berlin');
+// console.log(germany);
+
+// const finland = describeCountry('Finland', 25, 'Helsinki');
+// console.log(finland);
+
+// function calcAge1(birthYear) {
+//     return 2037 - birthYear;
+// }
+
+// const age1 = calcAge1(1991);
+
+
+// const calcAge2 = function (birthYear) {
+//     return 2037 - birthYear;
+// }
+// const age2 = calcAge2(1991);
+
+// console.log(age1, age2);
+
+// const population = 7900;
+// const countryPop = 1441;
+
+
+// function percentageOfWorld1(countryPop) {
+//     return (countryPop * 100) / population;
+// }
+// const percentage = percentageOfWorld1(countryPop);
+// console.log(percentage);
+// // 
+
+// const percentageOfWorld2 = function (countryPop) {
+//     return (countryPop * 100) / population;
+// }
+// const percentage2 = percentageOfWorld2(countryPop);
+// console.log(percentage2);
