9c8fd4b17c6c3708016c981fe53288b5a8476dd1
rayofhope50 <przemox2@gmail.com>
2023-01-02 20:38:16 +0000

    v123123

--- learning/udemy/jonas/02-Fundamentals-Part-2/starter/script.js
+++ learning/udemy/jonas/02-Fundamentals-Part-2/starter/script.js
@@ -340,3 +340,41 @@
 // for (let rep = 1; rep <= 10; rep++) {
 //     console.log(`Lifting weights repetition ${rep}`);
 // }
+const jonas = [
+    'Jonas',
+    'Schmedtmann',
+    2037 - 1991,
+    'teacher',
+    ['Michael', 'Peter', 'Steven']
+];
+// const types = [];
+
+// for (let i = 0; i <= jonas.length - 1; i++) {
+//     console.log(jonas[i], typeof jonas[i]);
+//     // types[i] = typeof jonas[i];
+//     types.push(typeof jonas[i]);
+// }
+
+// // console.log(jonas, types);
+
+// const years = [1991, 2007, 1969, 2020];
+// const ages = [];
+
+// for (let i = 0; i < years.length; i++) {
+//     ages.push(2023 - years[i]);
+//     console.log(ages);
+// }
+// console.log(years, ages);
+
+// /////////////// C O N T I N U E             B R E A K 
+// console.log('___ONLY STINGS___')
+// for (let i = 0; i <= jonas.length - 1; i++) {
+//     if (typeof jonas[i] !== 'string') continue;
+//     console.log(jonas[i], typeof jonas[i]);
+// }
+
+// console.log('___BREAK WITH NUMBER___')
+// for (let i = 0; i <= jonas.length - 1; i++) {
+//     if (typeof jonas[i] === 'number') break;
+//     console.log(jonas[i], typeof jonas[i]);
+// }
