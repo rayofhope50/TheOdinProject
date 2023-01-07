e5be7ea3e0355961c7644de7768b35e674b42882
rayofhope50 <przemox2@gmail.com>
2023-01-07 19:59:34 +0000

    practiced event listeners

--- learning/udemy/jonas/06-Modal/starter/script.js
+++ learning/udemy/jonas/06-Modal/starter/script.js
@@ -1,1 +1,32 @@
 'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+// function closing the modal
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+//function opening the modal
+const openModal = function () {
+  console.log('Button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+//function adding eventListener to all 3 buttons withe the same class
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+// close modal uppon clicking x
+btnCloseModal.addEventListener('click', closeModal);
+
+// close modal uppon clicking on background(overlay)
+overlay.addEventListener('click', closeModal);
+
+//keybord event listener for esc button to close the modal
+document.addEventListener('keydown', function (e) {
+  //   console.log(e.key);
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) closeModal();
+});
