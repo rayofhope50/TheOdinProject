5ac7d8c2e30d401f73ee92ae8e0f20e60b3da0ba
rayofhope50 <przemox2@gmail.com>
2023-02-07 20:04:14 +0000

    strings

--- learning/udemy/jonas/09-Data-Structures-Operators/starter/script.js
+++ learning/udemy/jonas/09-Data-Structures-Operators/starter/script.js
@@ -47,7 +47,87 @@ const restaurant = {
   },
   openingHours,
 };
+console.log('a+very+nice+string'.split('+'));
+console.log('Jonas Schmedtmann'.split(' '));
+const [firstName, lastName] = 'Jonas Schmedtmann'.split(' ');
+const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ');
+console.log(newName);
+// //// Working with Strings - Part 2
+// const airline = 'TAP Air Portugal';
 
+// console.log(airline.toLowerCase());
+// console.log(airline.toUpperCase());
+// //// Fix capitalisation in name
+// const passanger = 'jOnAS'; // Jonas
+// const passangerLower = passanger.toLowerCase();
+// const passangerCorrect =
+//   passangerLower[0].toLocaleUpperCase() + passangerLower.slice(1);
+// console.log(passangerCorrect);
+// // comparing email
+// const email = 'hello@jonas.io';
+// const loginEmail = '   Hello@Jonas.Io  \n';
+// const lowerEmail = loginEmail.toLowerCase();
+// const trimmedEmail = lowerEmail.trim();
+// console.log(trimmedEmail);
+
+// const normalizedEmail = loginEmail.toLowerCase().trim();
+// console.log(normalizedEmail);
+
+// //// replacing BUG
+// const priceGB = '288,97£';
+// const priceUS = priceGB.replace('£', '$').replace(',', '.');
+// console.log(priceUS);
+// const announcement =
+//   'All passengers come to boarding door 23. Boarding door 23!';
+// console.log(announcement.replaceAll('door', 'gate'));
+
+// //Booleans
+// const plane = 'Airbus A320neo';
+// console.log(plane.includes('A320'));
+// console.log(plane.includes('Boeing'));
+// console.log(plane.startsWith('Air'));
+// if (plane.startsWith('Airbus') && plane.endsWith('neo')) {
+//   console.log('Part of the NEW Airbus family');
+// }
+// // Example
+// const checkBaggage = function (items) {
+//   const baggage = items.toLowerCase();
+//   if (baggage.includes('knife') || baggage.includes('gun')) {
+//     console.log('You are NOT allowed on board');
+//   } else {
+//     console.log('Welcome aboard!');
+//   }
+// };
+// checkBaggage('I have a laptop, some Food and a pocket Knife');
+// checkBaggage('Socks and camera');
+// checkBaggage('got some snacks and a gun for protection');
+
+// /////////////// Working with strings part one BUG
+// const plane = 'A320';
+// console.log(plane[0]);
+// console.log(plane[1]);
+// console.log(plane[2]);
+// console.log('B737'[0]);
+// console.log(airline.length);
+// console.log('B737'.length);
+// console.log(airline.indexOf('r'));
+// console.log(airline.lastIndexOf('r'));
+// console.log(airline.indexOf('portugal'));
+// console.log(airline.slice(4));
+// console.log(airline.slice(4, 7));
+// console.log(airline.slice(0, airline.indexOf(' ')));
+// console.log(airline.slice(airline.lastIndexOf(' ') + 1));
+// console.log(airline.slice(-2));
+// console.log(airline.slice(1, -1));
+// const checkMiddleSeat = function (seat) {
+//   const s = seat.slice(-1);
+//   if (s === 'B' || s === 'E') console.log(`You got the middle seat!`);
+//   else console.log(`You got lucky`);
+// };
+
+// checkMiddleSeat('11B');
+// checkMiddleSeat('23C');
+// checkMiddleSeat('3E');
 // /////////// BUG MAPS FUNDAMENTALS 2
 // // setting a map all at once without using .set
 // const question = new Map([
