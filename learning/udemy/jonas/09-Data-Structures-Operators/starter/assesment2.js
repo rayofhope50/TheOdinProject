f3addee3dc329445b7d577d9c056065d92b0489b
rayofhope50 <przemox2@gmail.com>
2023-03-21 21:26:17 +0000

    done

--- learning/udemy/jonas/09-Data-Structures-Operators/starter/assesment2.js
+++ learning/udemy/jonas/09-Data-Structures-Operators/starter/assesment2.js
@@ -75,3 +75,42 @@ const game = {
 // // 7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
 
 // console.log(`The most likelly winner is ${team2 < team1 || game.team1}`);
+
+// Coding Challenge #2
+
+/* 
+
+
+BONUS: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
+      {
+        Gnarby: 1,
+        Hummels: 1,
+        Lewandowski: 2
+      }
+
+GOOD LUCK 😀
+*/
+// // 1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
+// for (const [position, scorrer] of game.scored.entries()) {
+//   console.log(`Goal ${position + 1}: ${scorrer}`);
+// }
+// // 2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
+// const odds = Object.values(game.odds);
+// let average = 0;
+// for (const odd of odds) {
+//   average += odd;
+// }
+// console.log(average / odds.length);
+// // 3. Print the 3 odds to the console, but in a nice formatted way, exaclty like this:
+// //       Odd of victory Bayern Munich: 1.33
+// //       Odd of draw: 3.25
+// //       Odd of victory Borrussia Dortmund: 6.5
+// // Get the team names directly from the game object, don't hardcode them (except for "draw"). HINT: Note how the odds and the game objects have the same property names 😉
+
+// for (const [team, odd] of Object.entries(game.odds)) {
+//   if (team == 'x') {
+//     console.log(`Odd of draw: ${odd}`);
+//   } else {
+//     console.log(`Odd of victory ${game[team]}: ${odd}`);
+//   }
+// }
--- learning/udemy/jonas/09-Data-Structures-Operators/starter/script2.js
+++ learning/udemy/jonas/09-Data-Structures-Operators/starter/script2.js
@@ -1,43 +1,41 @@
 'use strict';
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekdays[5]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-  order: function (starterIndex, mainIndex) {
+  order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
-  orderDelivery: function ({
-    starterIndex = 1,
-    mainIndex = 0,
-    time = '20:00',
-    address,
-  }) {
+  openingHours,
+
+  orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
     console.log(
       `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
     );
   },
-  orderPasta: function (ing1, ing2, ing3) {
+  orderPasta(ing1, ing2, ing3) {
     console.log(
       `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
     );
   },
-  orderPizza: function (mainIngredient, ...otherIngredients) {
+  orderPizza(mainIngredient, ...otherIngredients) {
     console.log(mainIngredient);
     console.log(otherIngredients);
   },
@@ -193,3 +191,60 @@ const restaurant = {
 // rest1.owner &&= 'Annonymus';
 // rest2.owner &&= 'Annonymus';
 // console.log(rest1.owner, rest2.owner);
+
+// // for of loop BUG BUG
+// const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+// for (const [i, el] of menu.entries()) console.log(`Position ${i + 1}: ${el}`);
+
+// // Object literal BUG BUG
+// const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+// for (const day of days) {
+//   const open = restaurant.openingHours[day]?.open ?? 'Closed';
+//   console.log(`On ${day}, we are open at ${open}`);
+// }
+// console.log(restaurant.order?.(0, 1) ?? 'Method thoes not exist');
+// console.log(restaurant.orderRISOTTO?.(0, 1) ?? 'Method thoes not exist');
+
+// // BUG Looping over obj
+// // Property names / keys
+// const properties = Object.keys(openingHours);
+// console.log(properties);
+// let openStr = `WE are open on ${properties.length} days: `;
+// for (const day of properties) {
+//   openStr += `${day},`;
+// }
+// console.log(openStr);
+// // Property Values
+// const values = Object.values(openingHours);
+// console.log(values);
+
+// // Entire Object
+// const entries = Object.entries(openingHours);
+// console.log(entries);
+
+// for (const [key, { open, close }] of entries) {
+//   console.log(`On ${key} we open ar ${open} and close at ${close}`);
+// }
+
+// BUG SETS BUG
+const orderSet = new Set([
+  'Pasta',
+  'Pizza',
+  'Risotto',
+  'Pasta',
+  'Pizza',
+  'Risotto',
+]);
+console.log(orderSet);
+console.log(new Set('Jonas'));
+console.log(orderSet.size);
+for (const order of orderSet) console.log(order);
+
+const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
+const staffUnique = [...new Set(staff)];
+console.log(staffUnique);
+
+console.log(
+  new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']).size
+);
+console.log(new Set('jonasschmedtmann').size);
