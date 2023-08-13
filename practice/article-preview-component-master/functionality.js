4f7a6aaf3fe79e556492174ae8d91d8e042d4d3d
Ray-Remote <przemox2@gmail.com>
2023-08-13 13:33:31 +0100

    fixed clicking on svg wrapper triggers js

--- practice/article-preview-component-master/functionality.js
+++ practice/article-preview-component-master/functionality.js
@@ -1,11 +1,11 @@
 "use strict";
-const shareBtn = document.getElementById("share");
+const shareBtn = document.getElementById("flex2");
 const socialActive = document.getElementById("social");
 const BtnVisible = document.getElementById("flex2");
 const SvgVisible = document.getElementsByClassName("share");
 
 shareBtn.addEventListener("click", () => {
   socialActive.classList.toggle("active");
-  //   BtnVisible.classList.toggle("active");
+  BtnVisible.classList.toggle("active");
   SvgVisible[0].classList.toggle("active");
 });
