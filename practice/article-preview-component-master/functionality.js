57858688f38048a952cc007466869eba44a7a765
Ray-Remote <przemox2@gmail.com>
2023-08-13 10:39:34 +0100

    mobile done

---
+++ practice/article-preview-component-master/functionality.js
@@ -0,0 +1,11 @@
+"use strict";
+const shareBtn = document.getElementById("share");
+const socialActive = document.getElementById("social");
+const BtnVisible = document.getElementById("flex2");
+const SvgVisible = document.getElementsByClassName("share");
+
+shareBtn.addEventListener("click", () => {
+  socialActive.classList.toggle("active");
+  BtnVisible.classList.toggle("active");
+  SvgVisible[0].classList.toggle("active");
+});
