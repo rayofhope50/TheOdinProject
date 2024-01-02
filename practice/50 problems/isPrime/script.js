83e09bb18c0b3aaf9fb928de8ef357b9c6a68d96
Ray-Remote <przemox2@gmail.com>
2024-01-02 19:08:59 +0000

    gogo

--- practice/50 problems/isPrime/script.js
+++ practice/50 problems/isPrime/script.js
@@ -1,8 +1,12 @@
-function myFunction(arr, num) {
-  return resultObj;
+function myFunction(a, n) {
+  let arr = [];
+  for (let i = n; i <= a.length; i += n) {
+    arr.push(a[i - 1]);
+  }
+  return arr;
 }
 
-console.log(myFunction([1, 2, 3], 6));
-console.log(myFunction(["a", "b"], 2));
-console.log(myFunction([null, false], 11));
+console.log(myFunction([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3));
+console.log(myFunction([10, 9, 8, 7, 6, 5, 4, 3, 2, 1], 5));
+console.log(myFunction([7, 2, 1, 6, 3, 4, 5, 8, 9, 10], 2));
 // console.log(myFunction(-5, 7));
