69dd43232d270003a20b930814803faf73c15dea
Ray-Remote <przemox2@gmail.com>
2024-01-08 21:21:21 +0000

    gogo

--- practice/50 problems/isPrime/script.js
+++ practice/50 problems/isPrime/script.js
@@ -1,12 +1,12 @@
 function myFunction(a) {
-  let result = Object.entries(a).reduce((obj, [key, val]) => {
-    obj[val] = key;
-    return obj;
+  let result = Object.entries(a).reduce((l, [key, val], k) => {
+    l[key] = val == "" || val == " " ? null : val;
+    return l;
   }, {});
   return result;
 }
 
-console.log(myFunction({ z: "a", y: "b", x: "c", w: "d" }));
-console.log(myFunction({ 2: "a", 4: "b", 6: "c", 8: "d" }));
-console.log(myFunction({ a: 1, z: 24 }));
+console.log(myFunction({ a: "a", b: "b", c: "" }));
+console.log(myFunction({ a: "", b: "b", c: " ", d: "d" }));
+console.log(myFunction({ a: "a", b: "b ", c: " ", d: "" }));
 // console.log(myFunction(-5, 7));
