308507bd4b74afc4bcd54941b1c4fb3bb91ff89e
Ray-Remote <przemox2@gmail.com>
2024-01-09 20:55:20 +0000

    gogo

--- practice/50 problems/isPrime/script.js
+++ practice/50 problems/isPrime/script.js
@@ -1,32 +1,14 @@
-function myFunction(obj) {
-  let newObj = { ...obj };
-  delete newObj.age;
-  delete newObj.email;
-  for (key in newObj) {
-    if (key == "size") {
-      newObj[key] = `${String(newObj[key])}cm`;
-    }
-    if (key == "weight") {
-      newObj[key] = `${String(newObj[key])}kg`;
-    }
-  }
+function myFunction(a) {
+  a.reduce((acc, val, i) => {
+    acc[val];
 
-  return newObj;
+    console.log(acc);
+    console.log(val);
+    console.log(i);
+    return acc;
+  }, {});
+  return a;
 }
 
-console.log(
-  myFunction({ fn: "Lisa", ln: "Müller", age: 17, size: 175, weight: 67 })
-);
-console.log(
-  myFunction({
-    fn: "Martin",
-    ln: "Harper",
-    age: 26,
-    email: "martin.harper@test.de",
-    weight: 102,
-  })
-);
-console.log(
-  myFunction({ fn: "Andrew", ln: "Harper", age: 81, size: 175, weight: 71 })
-);
-// console.log(myFunction(-5, 7));
+console.log(myFunction([1, 2, 2, 3]));
+console.log(myFunction([9, 9, 9, 99]));
