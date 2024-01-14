e102cfeb203bd2a0e580c7828b2b911fbf1052b4
Ray-Remote <przemox2@gmail.com>
2024-01-14 16:40:47 +0000

    Created a function tthat takes a date and tells the next quarter hour value example 15:01 result 15

--- practice/50 problems/isPrime/script.js
+++ practice/50 problems/isPrime/script.js
@@ -1,11 +1,19 @@
-function myFunction(a, b) {
-  let diff, hrs, min, sec;
-  diff = Math.abs(a - b);
-  hrs = Math.floor(diff / 60 / 60 / 1000);
-  min = Math.floor((diff / 60 / 1000) % 60);
-  sec = Math.floor((diff / 1000) % 60);
-  // console.log(diff, hours, min, sec);
-  return { hrs, min, sec };
+function myFunction(a) {
+  // Takes number of minutes from date 31 min
+  let min = a.getMinutes();
+  // % of 31/ 15 = 1 reminder is 1
+  let reminder = min % 15;
+  // looks for number of minutes missing to full quarter which is
+  // every 15 min that's why 15 - 1 = 14 beacuse 31 + 14 equalls 45
+  // which we are looking for
+  let minutesToAdd = reminder === 0 ? 0 : 15 - reminder;
+  // creates a new date 15:31 which we can modify
+  let nextQuarterHour = new Date(a);
+  // modifies minutes in new Date to 31 + 14 from previous examples to make it full 45
+  nextQuarterHour.setMinutes(min + minutesToAdd);
+  // it reads minutes from date we setted up in the previous step and
+  // returns it
+  return nextQuarterHour.getMinutes();
 }
 
 console.log(myFunction(new Date(2021, 8, 10, 15, 14, 00)));
