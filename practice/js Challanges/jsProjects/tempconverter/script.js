ca9eae62165d5a2653295b80d5dd98192243de1c
Ray-Remote <przemox2@gmail.com>
2024-01-23 09:53:03 +0000

    tempConv finished canky but working. not onto the upgrades

--- practice/js Challanges/jsProjects/tempconverter/script.js
+++ practice/js Challanges/jsProjects/tempconverter/script.js
@@ -1,16 +1,53 @@
 const inputs = document.querySelectorAll("input");
 const result = document.getElementById("result");
+const inputC = document.getElementById("inputC");
+const inputF = document.getElementById("inputF");
+const inputK = document.getElementById("inputK");
 // [0] is C [1] is F [2] is K
 let temps = [];
 inputs.forEach((input) => {
-  input.addEventListener("input", () => {
-    const id = input.id;
-    calcFahre(input.value);
-    console.log(temps);
+  input.addEventListener("focus", () => {
+    input.value = "";
+    input.addEventListener("blur", () => {
+      const inputVal = input.value;
+      if (inputVal !== "" && !isNaN(inputVal)) {
+        const inputId = input.id;
+        if (input.id == "inputC") {
+          calcCel(inputVal);
+        } else if (input.id == "inputF") {
+          calcFahre(inputVal);
+        } else {
+          calcKel(inputVal);
+        }
+
+        console.log(temps);
+      }
+    });
   });
 });
 
+function calcCel(temp) {
+  temps = [];
+  temp = parseFloat(temp);
+  temps[1] = ((temp * 9) / 5 + 32).toFixed(2);
+  temps[2] = (temp + 273.15).toFixed(2);
+  inputF.value = temps[1];
+  inputK.value = temps[2];
+}
+
 function calcFahre(temp) {
-  temps[0].push((temp * 9) / 5 + 32);
-  temps[2].push(((temp - 273.15) * 9) / 5 + 32);
+  temps = [];
+  temp = parseFloat(temp);
+  temps[0] = (((temp - 32) * 5) / 9).toFixed(2);
+  temps[2] = (((temp - 32) * 5) / 9 + 273.15).toFixed(2);
+  inputC.value = temps[0];
+  inputK.value = temps[2];
+}
+function calcKel(temp) {
+  temps = [];
+  temp = parseFloat(temp);
+  temps[0] = (temp - 273.15).toFixed(2);
+  temps[1] = (((temp - 273.15) * 9) / 5 + 32).toFixed(2);
+  inputC.value = temps[0];
+  inputF.value = temps[1];
 }
