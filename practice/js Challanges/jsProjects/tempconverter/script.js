312318b67bd2f741dde7f4f979110081c4e403af
Ray-Remote <przemox2@gmail.com>
2024-01-23 10:14:32 +0000

    created isValidNumber and updateTemps changing handling of different cases depending on button click rather than nested if statements

--- practice/js Challanges/jsProjects/tempconverter/script.js
+++ practice/js Challanges/jsProjects/tempconverter/script.js
@@ -1,34 +1,45 @@
+"use strict";
 const inputs = document.querySelectorAll("input");
-const result = document.getElementById("result");
 const inputC = document.getElementById("inputC");
 const inputF = document.getElementById("inputF");
 const inputK = document.getElementById("inputK");
+const result = document.getElementById("result");
 // [0] is C [1] is F [2] is K
 let temps = [];
 inputs.forEach((input) => {
+  // this clears the input tha's been clicked on
   input.addEventListener("focus", () => {
     input.value = "";
-    input.addEventListener("blur", () => {
-      const inputVal = input.value;
-      if (inputVal !== "" && !isNaN(inputVal)) {
-        const inputId = input.id;
-        if (input.id == "inputC") {
-          calcCel(inputVal);
-        } else if (input.id == "inputF") {
-          calcFahre(inputVal);
-        } else {
-          calcKel(inputVal);
-        }
-
-        console.log(temps);
-      }
-    });
+  });
+  // main calc algo
+  input.addEventListener("blur", () => {
+    const inputVal = input.value;
+    if (isValidNumber(inputVal)) {
+      updateTemps(input.id, parseFloat(inputVal));
+    }
   });
 });
 
+function updateTemps(inputId, temp) {
+  switch (inputId) {
+    case "inputC":
+      calcCel(temp);
+      break;
+    case "inputF":
+      calcFahre(temp);
+      break;
+    case "inputK":
+      calcKel(temp);
+      break;
+    default:
+      break;
+  }
+}
+
+function isValidNumber(value) {
+  return value !== "" && !isNaN(value);
+}
 function calcCel(temp) {
-  temps = [];
-  temp = parseFloat(temp);
   temps[1] = ((temp * 9) / 5 + 32).toFixed(2);
   temps[2] = (temp + 273.15).toFixed(2);
   inputF.value = temps[1];
@@ -36,16 +47,12 @@ function calcCel(temp) {
 }
 
 function calcFahre(temp) {
-  temps = [];
-  temp = parseFloat(temp);
   temps[0] = (((temp - 32) * 5) / 9).toFixed(2);
   temps[2] = (((temp - 32) * 5) / 9 + 273.15).toFixed(2);
   inputC.value = temps[0];
   inputK.value = temps[2];
 }
 function calcKel(temp) {
-  temps = [];
-  temp = parseFloat(temp);
   temps[0] = (temp - 273.15).toFixed(2);
   temps[1] = (((temp - 273.15) * 9) / 5 + 32).toFixed(2);
   inputC.value = temps[0];
