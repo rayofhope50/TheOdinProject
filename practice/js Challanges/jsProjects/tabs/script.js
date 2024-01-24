098f9457a532d42401aae1891f8be2accca3b490
Ray-Remote <przemox2@gmail.com>
2024-01-24 19:36:40 +0000

    tabs done, clever solution to ID problem, when one tab is clicked linked  article  by data-id and id will reveal itelf

--- practice/js Challanges/jsProjects/tabs/script.js
+++ practice/js Challanges/jsProjects/tabs/script.js
@@ -1,13 +1,44 @@
 "use strict";
-const buttons = document.querySelectorAll("button");
-const step1 = document.getElementById("step1");
-const step2 = document.getElementById("step2");
-const step3 = document.getElementById("step3");
+const buttons = document.querySelectorAll(".button");
+const tabs = document.querySelector(".tabs");
+const articles = document.querySelectorAll(".content");
 
-buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    const btn = button.dataset.id;
-    console.log(btn);
-    console.log(document.getElementById(btn));
-  });
+// Clever trick it check ID by target which data- id put on a button
+// and associated with article which actually has that ID targets it
+// and modifies
+tabs.addEventListener("click", function (element) {
+  const id = element.target.dataset.id;
+  console.log(id);
+  if (id) {
+    buttons.forEach((btn) => {
+      btn.classList.remove("live");
+    });
+    element.target.classList.add("live");
+
+    articles.forEach(function (article) {
+      article.classList.remove("live");
+    });
+    const e = document.getElementById(id);
+    e.classList.add("live");
+  }
 });
+
+// const tabs = document.querySelector(".tabs");
+// const btns = document.querySelectorAll(".button");
+// const articles = document.querySelectorAll(".content");
+// tabs.addEventListener("click", function (e) {
+//   const id = e.target.dataset.id;
+//   if (id) {
+//     // remove selected from other buttons
+//     btns.forEach(function (btn) {
+//       btn.classList.remove("live");
+//     });
+//     e.target.classList.add("live");
+//     // hide other articles
+//     articles.forEach(function (article) {
+//       article.classList.remove("live");
+//     });
+//     const element = document.getElementById(id);
+//     element.classList.add("live");
+//   }
+// });
