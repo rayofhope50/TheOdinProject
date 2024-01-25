edde28ec9b7038c9027f7968cb987dcbe1dd3a93
Ray-Remote <przemox2@gmail.com>
2024-01-25 20:47:33 +0000

    this is suprisingly difficult i'm sure there's way easier way but I'm learning and making progress

--- practice/js Challanges/jsProjects/progressBar/script.js
+++ practice/js Challanges/jsProjects/progressBar/script.js
@@ -3,6 +3,8 @@ const steps = document.querySelectorAll(".step");
 const btns = document.querySelectorAll(".btn");
 const btnWrapp = document.querySelector(".cont__btn__wrap");
 const progressTop = document.getElementById("progressTop");
+const ins = document.querySelectorAll(".fas");
+let i = 1;
 
 btns.forEach((btn) => {
   btn.addEventListener("click", function () {
@@ -11,18 +13,37 @@ btns.forEach((btn) => {
     } else {
       back();
     }
-    console.log(btn.id);
   });
 });
 function forward() {
-  // steps.forEach(step => {
-  //   if (step.classList.contains("checked")) {
-  //     step.classList
-  //   }
-  // })
-  console.log(`${steps[2].inner}`);
+  if (i == 4) {
+    btns[1].classList.add("checked__Btn");
+    console.log("End");
+  }
+  if (i < steps.length) {
+    steps[i].classList.add("checked");
+    ins[i].classList.remove("fa-times");
+    ins[i].classList.add("fa-check");
+    steps[i];
+    i++;
+    btns[0].classList.remove("checked__Btn");
+    console.log(i);
+    return;
+  }
 }
 
 function back() {
-  console.log(`buttons should move back`);
+  if (i == 2) {
+    btns[0].classList.add("checked__Btn");
+    console.log("END");
+  }
+  if (i > 1) {
+    i--;
+    steps[i].classList.remove("checked");
+    btns[1].classList.remove("checked__Btn");
+    ins[i].classList.remove("fa-check");
+    ins[i].classList.add("fa-times");
+    console.log(i);
+    return;
+  }
 }
