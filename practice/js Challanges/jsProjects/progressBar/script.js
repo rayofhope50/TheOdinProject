c2e127ccf6f9c262350841433f0503082c527c12
Ray-Remote <przemox2@gmail.com>
2024-01-25 12:56:05 +0000

    working out how to ittirate over steps

--- practice/js Challanges/jsProjects/progressBar/script.js
+++ practice/js Challanges/jsProjects/progressBar/script.js
@@ -1,1 +1,28 @@
 "use strict";
+const steps = document.querySelectorAll(".step");
+const btns = document.querySelectorAll(".btn");
+const btnWrapp = document.querySelector(".cont__btn__wrap");
+const progressTop = document.getElementById("progressTop");
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    if (btn.id == "next") {
+      forward();
+    } else {
+      back();
+    }
+    console.log(btn.id);
+  });
+});
+function forward() {
+  // steps.forEach(step => {
+  //   if (step.classList.contains("checked")) {
+  //     step.classList
+  //   }
+  // })
+  console.log(`${steps[2].inner}`);
+}
+
+function back() {
+  console.log(`buttons should move back`);
+}
