3a290281ef4eebb75317e5f0fbdc6eece128b6f5
Ray-Remote <przemox2@gmail.com>
2024-01-29 12:58:14 +0000

    js half ways

--- practice/js Challanges/jsProjects/socialMediaList/script.js
+++ practice/js Challanges/jsProjects/socialMediaList/script.js
@@ -1,1 +1,8 @@
 "use strict";
+const arrow = document.querySelector(".fas");
+const list = document.querySelector(".media__list");
+
+arrow.addEventListener("click", () => {
+  arrow.classList.add("rotate");
+  list.classList.remove("hide");
+});
