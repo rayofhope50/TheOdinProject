035b4d388a3e7ed6fcf27f6f89472237f383fb8e
Ray-Remote <przemox2@gmail.com>
2024-01-30 12:44:44 +0000

    back to work

--- practice/js Challanges/jsProjects/sidebar/script.js
+++ practice/js Challanges/jsProjects/sidebar/script.js
@@ -1,1 +1,12 @@
 "use strict";
+const navBar = document.querySelector(".nav__bar");
+const burgerBtn = document.querySelector(".fa-bars");
+const xBtn = document.querySelector(".fa-times");
+
+burgerBtn.addEventListener("click", () => {
+  if (!navBar.classList.contains("checked")) {
+    navBar.classList.add("checked");
+  } else {
+    navBar.classList.remove("checked");
+  }
+});
