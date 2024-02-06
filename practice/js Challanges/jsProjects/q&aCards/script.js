ed65580a5237c017ef203332e694479a95806285
Ray-Remote <przemox2@gmail.com>
2024-02-06 12:24:31 +0000

    gogo

--- practice/js Challanges/jsProjects/q&aCards/script.js
+++ practice/js Challanges/jsProjects/q&aCards/script.js
@@ -5,21 +5,12 @@ questions.forEach((question) => {
   const i = question.querySelector("i");
   const text = question.querySelector(".question__text");
   question.addEventListener("click", function () {
-    if (i.classList.contains("fa-plus-square")) {
-      removePlus(i, text);
-    } else {
-      removeMinus(i, text);
-    }
+    removePlus(i, text);
   });
 });
 
 function removePlus(i, text) {
-  i.classList.add("fa-minus-square");
-  i.classList.remove("fa-plus-square");
-  text.classList.remove("hidden");
-}
-function removeMinus(i, text) {
-  i.classList.remove("fa-minus-square");
-  i.classList.add("fa-plus-square");
-  text.classList.add("hidden");
+  i.classList.toggle("fa-minus-square");
+  i.classList.toggle("fa-plus-square");
+  text.classList.toggle("hidden");
 }
---
+++ practice/js Challanges/jsProjects/spinningDigitEffect/script.js
@@ -0,0 +1,21 @@
+"use strict";
+const texts = document.querySelectorAll(".stats__text");
+texts.forEach((text) => {
+  counter(text);
+});
+function counter(text) {
+  let i = 1;
+  const count = parseInt(text.innerText, 10);
+  let increment = Math.ceil(count / 15);
+
+  function incrementCounter() {
+    if (i < count) {
+      text.innerText = `${i}`;
+      i += increment;
+      setTimeout(incrementCounter, 50);
+    } else {
+      text.innerText = `${count}`;
+    }
+  }
+  incrementCounter();
+}
