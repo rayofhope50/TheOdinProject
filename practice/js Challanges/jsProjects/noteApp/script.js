2964eed88f7aa6d2263ea465f9d5613ea38be56b
Ray-Remote <przemox2@gmail.com>
2024-02-12 12:05:53 +0000

    app done without json local memory will to that later once i get to know json

--- practice/js Challanges/jsProjects/noteApp/script.js
+++ practice/js Challanges/jsProjects/noteApp/script.js
@@ -1,6 +1,7 @@
 "use strict";
 const addBtn = document.getElementById("addBtn");
 const notes = document.getElementById("notes");
+const note = document.querySelectorAll(".notes__input");
 
 addBtn.addEventListener("click", function () {
   addText();
@@ -10,5 +11,15 @@ function addText() {
   const newEl = document.createElement("textarea");
   newEl.classList.add("notes__input");
   notes.appendChild(newEl);
+
+  newEl.addEventListener("dblclick", function () {
+    remove(this);
+  });
 }
-// end
+
+function remove(element) {
+  const info = confirm("Are you sure you want to remove it?");
+  if (info) {
+    element.remove();
+  }
+}
