c0ae881711365a373c7da94caf2c55ca4a6fd4e0
Ray-Remote <przemox2@gmail.com>
2024-02-14 11:40:30 +0000

    project finished i found that animation breaks down when toggling between height of fit-content and a number. Got rid off fit-content and works great

--- practice/js Challanges/jsProjects/responsiveNav/script.js
+++ practice/js Challanges/jsProjects/responsiveNav/script.js
@@ -2,4 +2,10 @@
 const nav = document.querySelector("nav");
 const btn = document.getElementById("btn");
 
-btn.addEventListener("click", () => {});
+btn.addEventListener("click", () => {
+  if (nav.classList.contains("disabled")) {
+    nav.classList.remove("disabled");
+  } else {
+    nav.classList.add("disabled");
+  }
+});
