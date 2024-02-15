0a350dba7a2ce7c175b232db89f8d9de0207a071
Ray-Remote <przemox2@gmail.com>
2024-02-15 12:40:41 +0000

    gogo

---
+++ practice/js Challanges/jsProjects/miniCalendar/script.js
@@ -0,0 +1,8 @@
+"use strict";
+const month = document.querySelector(".header");
+const dayTxt = document.querySelector(".day__txt");
+const dayNr = document.querySelector(".day__nr");
+const year = document.querySelector(".year");
+const today = new Date();
+
+console.log(today.getDate());
