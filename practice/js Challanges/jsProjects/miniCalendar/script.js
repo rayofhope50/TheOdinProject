a164136edd479b58f3c2b05c63ec4af09599d4fb
Ray-Remote <przemox2@gmail.com>
2024-02-18 19:42:22 +0000

    done

--- practice/js Challanges/jsProjects/miniCalendar/script.js
+++ practice/js Challanges/jsProjects/miniCalendar/script.js
@@ -4,6 +4,32 @@ const dayTxt = document.querySelector(".day__txt");
 const dayNr = document.querySelector(".day__nr");
 const year = document.querySelector(".year");
 const today = new Date();
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "Osctober",
+  "November",
+  "December",
+];
 //
 //
-console.log(today.getDate());
+dayNr.innerText = today.getDate();
+year.innerText = today.getFullYear();
+dayTxt.innerText = days[today.getDay()];
+month.innerText = months[today.getMonth()];
