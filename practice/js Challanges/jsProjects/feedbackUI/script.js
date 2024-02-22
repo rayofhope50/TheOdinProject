17854c2331f65cb9c2d2810cf3892e3a7e17596e
Ray-Remote <przemox2@gmail.com>
2024-02-22 18:07:54 +0000

    all done e.target very interesting

--- practice/js Challanges/jsProjects/feedbackUI/script.js
+++ practice/js Challanges/jsProjects/feedbackUI/script.js
@@ -1,9 +1,30 @@
 "use strict";
 const btn = document.getElementById("btn");
 const icons = document.querySelectorAll(".fe__icon");
-
+const wrap = document.querySelector(".wrap");
+let rating = "";
 icons.forEach((icon, idx, k) => {
-  icon.addEventListener("click", function () {
-    console.log(`hllo ${idx} ${k}`);
+  icon.addEventListener("click", function (e) {
+    removeActive();
+    rating = e.target.innerText;
+    icons[idx].classList.add("active");
   });
+});
+
+function removeActive() {
+  icons.forEach((icon) => {
+    icon.classList.remove("active");
+  });
+}
+
+btn.addEventListener("click", function () {
+  wrap.innerHTML = `
+  <h2 class="fe__h1">Thank you!</h2>
+  <br>
+
+  <h2 class="fe__h1">Feedback: ${rating} </h2>
+  <br>
+  <br>
+  <p class="fe__icon_p2">We'll use your feedback to improve our customer support.</p>  
+  `;
 });
