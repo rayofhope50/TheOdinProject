97465807f7fcc4ad7cbb0b1d922b01d34dca5287
Ray-Remote <przemox2@gmail.com>
2024-02-28 11:12:17 +0000

    finished project learned a lot about audio, made it scalable by using variables so as menny buttons and sounds as you like

--- practice/js Challanges/jsProjects/drumKits/script.js
+++ practice/js Challanges/jsProjects/drumKits/script.js
@@ -2,8 +2,40 @@
 const kits = ["crash", "kick", "snare", "tom"];
 const parent = document.querySelector(".container");
 
-const el = document.createElement("button");
-el.classList.add("btn");
-parent.appendChild(el);
-el.innerText = `${kits[0][0].toUpperCase() + kits[0].slice(1)}`;
-el.style.backgroundImage = `url(./pictures/${kits[0]}.png)`;
+kits.forEach((kit, idx) => {
+  const btnEl = ceateBtn(idx);
+  createSound(btnEl, kit, idx);
+});
+
+function ceateBtn(idx) {
+  const el = document.createElement("button");
+  el.classList.add("btn");
+  parent.appendChild(el);
+  el.innerText = `${kits[idx][0].toUpperCase() + kits[idx].slice(1)}`;
+  el.style.backgroundImage = `url(./pictures/${kits[idx]}.png)`;
+  return el;
+}
+
+function createSound(btnEl, kit, idx) {
+  const audio = document.createElement("audio");
+  audio.src = `./sounds/${kits[idx]}.mp3`;
+  window.addEventListener("keydown", function (e) {
+    if (e.key === kit.slice(0, 1)) {
+      console.log(`audio start`);
+      if (audio.paused) {
+        console.log(`audio pause`);
+        audio.play();
+      } else {
+        audio.pause();
+        audio.currentTime = 0;
+      }
+      audio.play();
+
+      btnEl.style.transform = "scale(.9)";
+      setTimeout(() => {
+        btnEl.style.transform = "scale(1)";
+      }, 100);
+    }
+  });
+  parent.appendChild(audio);
+}
