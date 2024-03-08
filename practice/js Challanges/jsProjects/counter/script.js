d10bec944b197c56b7e425aaf2073bdbfc9fed97
Ray-Remote <przemox2@gmail.com>
2024-03-08 10:25:30 +0000

    gpg

--- practice/js Challanges/jsProjects/counter/script.js
+++ practice/js Challanges/jsProjects/counter/script.js
@@ -1,1 +1,29 @@
 "use strict";
+const counter = document.querySelector(".number");
+const btns = document.querySelectorAll(".btn");
+
+let num = 0;
+
+btns.forEach((btn, idx) => {
+  btn.addEventListener("click", () => {
+    if (idx == 0) {
+      num--;
+    } else if (idx == `1`) {
+      num = 0;
+    } else {
+      num++;
+    }
+    counter.innerText = `${num}`;
+    colorChange();
+  });
+});
+
+function colorChange() {
+  if (num < 0) {
+    counter.style.color = `red`;
+  } else if (num > 0) {
+    counter.style.color = `green`;
+  } else {
+    counter.style.color = `black`;
+  }
+}
