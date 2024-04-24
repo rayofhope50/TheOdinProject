b4e8ca7b279180d42d601ec091deaf897ed2a14b
Ray-Remote <przemox2@gmail.com>
2024-04-24 11:56:02 +0100

    gogo

--- practice/superSimpleDev/scripts/12-rock-paper-scissors.js
+++ practice/superSimpleDev/scripts/12-rock-paper-scissors.js
@@ -10,7 +10,7 @@ let intervalId;
 
 function autoPlay() {
   if (!isAutoPlaying) {
-    intervalId = setInterval(function () {
+    intervalId = setInterval(() => {
       const playerMove = pickComputerMove();
 
       playGame(playerMove);
