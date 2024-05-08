3400e722b93a29f591c17eae244f610b61da88a3
Ray-Remote <przemox2@gmail.com>
2024-05-08 13:52:02 +0100

    gogo

--- practice/superSimpleDev/13-javascript-amazon-project/data/cart.js
+++ practice/superSimpleDev/13-javascript-amazon-project/data/cart.js
@@ -1,1 +1,20 @@
 export const cart = [];
+
+export function addToCart(productId, productQuantity) {
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  if (matchingItem) {
+    matchingItem.quantity += Number(productQuantity.value);
+  } else {
+    cart.push({
+      productId,
+      quantity: Number(productQuantity.value),
+    });
+  }
+}
