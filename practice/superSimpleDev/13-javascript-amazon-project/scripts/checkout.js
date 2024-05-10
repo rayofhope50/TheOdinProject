0b43a5e67306cc3de16e47cdb69e97b56a1d6c5b
Ray-Remote <przemox2@gmail.com>
2024-05-10 11:46:33 +0100

    gogo

--- practice/superSimpleDev/13-javascript-amazon-project/scripts/checkout.js
+++ practice/superSimpleDev/13-javascript-amazon-project/scripts/checkout.js
@@ -1,6 +1,7 @@
 "use strict";
 import { cart } from "../data/cart.js";
 import { products } from "../data/products.js";
+let cartSummaryHTML = "";
 
 cart.forEach((cartItem) => {
   const productId = cartItem.productId;
@@ -11,26 +12,26 @@ cart.forEach((cartItem) => {
       matchingProduct = product;
     }
   });
-  console.log(matchingProduct);
-  `
+
+  cartSummaryHTML += `
     <div class="cart-item-container">
     <div class="delivery-date">
         Delivery date: Tuesday, June 21
     </div>
 
     <div class="cart-item-details-grid">
-        <img class="product-image" src="images/products/athletic-cotton-socks-6-pairs.jpg">
+        <img class="product-image" src="${matchingProduct.image}">
 
         <div class="cart-item-details">
         <div class="product-name">
-            Black and Gray Athletic Cotton Socks - 6 Pairs
+            ${matchingProduct.name}
         </div>
         <div class="product-price">
-            $10.90
+            $${matchingProduct.priceCents / 100}
         </div>
         <div class="product-quantity">
             <span>
-            Quantity: <span class="quantity-label">2</span>
+            Quantity: <span class="quantity-label">${cartItem.quantity}</span>
             </span>
             <span class="update-quantity-link link-primary">
             Update
@@ -83,3 +84,5 @@ cart.forEach((cartItem) => {
     </div>
   `;
 });
+
+document.querySelector(".js-order-summary").innerHTML = cartSummaryHTML;
