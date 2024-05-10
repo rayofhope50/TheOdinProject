db10313610ef4c43716f3581d6b4835bc8d3aa35
Ray-Remote <przemox2@gmail.com>
2024-05-10 13:28:46 +0100

    save cart in local storage

--- practice/superSimpleDev/13-javascript-amazon-project/data/cart.js
+++ practice/superSimpleDev/13-javascript-amazon-project/data/cart.js
@@ -1,14 +1,19 @@
-export let cart = [
-  {
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 2,
-  },
-  {
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 1,
-  },
-];
-
+export let cart = JSON.parse(localStorage.getItem("cart"));
+if (!cart) {
+  [
+    {
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 2,
+    },
+    {
+      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      quantity: 1,
+    },
+  ];
+}
+function saveToStorage() {
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
 export function addToCart(productId, productQuantity) {
   let matchingItem;
 
@@ -26,6 +31,7 @@ export function addToCart(productId, productQuantity) {
       quantity: Number(productQuantity.value),
     });
   }
+  saveToStorage();
 }
 
 export function removeFromCart(productId) {
@@ -34,6 +40,8 @@ export function removeFromCart(productId) {
     if (cartItem.productId !== productId) {
       newCart.push(cartItem);
     }
-    cart = newCart;
   });
+  cart = newCart;
+
+  saveToStorage();
 }
