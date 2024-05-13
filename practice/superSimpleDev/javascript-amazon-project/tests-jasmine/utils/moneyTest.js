98ee948b6bd4a1fc415b4d79b28d2417503cc6fa
Ray-Remote <przemox2@gmail.com>
2024-05-13 13:52:40 +0100

    break, begining of cartTest

--- practice/superSimpleDev/javascript-amazon-project/tests-jasmine/utils/moneyTest.js
+++ practice/superSimpleDev/javascript-amazon-project/tests-jasmine/utils/moneyTest.js
@@ -1,4 +1,4 @@
-import formatCurrency from "../scripts/utils/money.js";
+import formatCurrency from "../../scripts/utils/money.js";
 // descibe is a suite
 describe("test suite: formatCurrency", () => {
   // it is a test
