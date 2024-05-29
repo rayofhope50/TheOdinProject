d6c474e22dc282c0e1cd551eb6074ce825bf2094
Ray-Remote <przemox2@gmail.com>
2024-05-29 11:08:12 +0100

    create oop version cart

--- practice/superSimpleDev/javascript-EXERCISES/tests/checkout/orderSummaryTest.js
+++ practice/superSimpleDev/javascript-EXERCISES/tests/checkout/orderSummaryTest.js
@@ -2,15 +2,16 @@ import { renderOrderSummary } from "../../scripts/checkout/orderSummary.js";
 import { loadFromStorage, cart } from "../../data/cart.js";
 
 describe("test suite: renderOrderSummary", () => {
-  const productId1 = "e43638ce-6aa0-4b85-b27f-e1d07eb678c6";
-  const productId2 = "15b6fc6f-327a-4ec4-896f-486349e85a3d";
   beforeEach(() => {
-    spyOn(localStorage, "setItem");
-
     document.querySelector(".js-test-container").innerHTML = `
     <div class="js-order-summary" ></div>
     <div class="js-payment-summary"></div>
   `;
+  });
+  const productId1 = "e43638ce-6aa0-4b85-b27f-e1d07eb678c6";
+  const productId2 = "15b6fc6f-327a-4ec4-896f-486349e85a3d";
+  beforeEach(() => {
+    spyOn(localStorage, "setItem");
 
     spyOn(localStorage, "getItem").and.callFake(() => {
       return JSON.stringify([
@@ -40,8 +41,15 @@ describe("test suite: renderOrderSummary", () => {
     expect(
       document.querySelector(`.js-product-quantity-${productId2}`).innerText
     ).toContain("Quantity: 1");
+    expect(
+      document.querySelector(`.js-product-name-${productId1}`).innerText
+    ).toEqual("Black and Gray Athletic Cotton Socks - 6 Pairs");
+    expect(
+      document.querySelector(`.js-product-name-${productId2}`).innerText
+    ).toEqual("Intermediate Size Basketball");
   });
   document.querySelector(".js-test-container").innerHTML = ``;
+
   it("removes a product", () => {
     document.querySelector(`.js-delete-link-${productId1}`).click();
     expect(document.querySelectorAll(".js-cart-item-container").length).toEqual(
@@ -53,8 +61,16 @@ describe("test suite: renderOrderSummary", () => {
     expect(
       document.querySelector(`.js-cart-item-container-${productId2}`)
     ).not.toEqual(null);
+    expect(
+      document.querySelector(`.js-product-name-${productId2}`).innerText
+    ).toEqual("Intermediate Size Basketball");
     expect(cart.length).toEqual(1);
     expect(cart[0].productId).toEqual(productId2);
     document.querySelector(".js-test-container").innerHTML = ``;
+  });
+  it("checks if product prices contain $", () => {
+    expect(
+      document.querySelector(`.js-product-price-${productId1}`).innerText
+    ).toEqual("$10.90");
   });
 });
