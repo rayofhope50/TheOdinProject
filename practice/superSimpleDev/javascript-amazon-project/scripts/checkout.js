009c40e23286a361f4afc685690d35b30b45b3ac
Ray-Remote <przemox2@gmail.com>
2024-06-11 09:03:40 +0100

    learn promises and convert code to promises

--- practice/superSimpleDev/javascript-amazon-project/scripts/checkout.js
+++ practice/superSimpleDev/javascript-amazon-project/scripts/checkout.js
@@ -5,22 +5,40 @@ import { loadCart } from "../data/cart.js";
 // import "../data/cart-class.js";
 // import "../data/backend-practice.js";
 
-new Promise((resolve) => {
-  loadProducts(() => {
-    resolve();
-  });
-})
-  .then(() => {
-    return new Promise((resolve) => {
-      loadCart(() => {
-        resolve();
-      });
+Promise.all([
+  new Promise((resolve) => {
+    loadProducts(() => {
+      resolve("value1");
     });
-  })
-  .then(() => {
-    renderOrderSummary();
-    renderPaymentSummary();
-  });
+  }),
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  }),
+]).then((values) => {
+  console.log(values);
+  renderOrderSummary();
+  renderPaymentSummary();
+});
+
+// new Promise((resolve) => {
+//   loadProducts(() => {
+//     resolve("value1");
+//   });
+// })
+//   .then((value) => {
+//     console.log(value);
+//     return new Promise((resolve) => {
+//       loadCart(() => {
+//         resolve();
+//       });
+//     });
+//   })
+//   .then(() => {
+//     renderOrderSummary();
+//     renderPaymentSummary();
+//   });
 
 // loadProducts(() => {
 //   loadCart(() => {
