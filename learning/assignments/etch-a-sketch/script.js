e795632180e3ec5f88858a5dd18755a9b0e52fd5
Ray-Remote <przemox2@gmail.com>
2024-06-18 14:10:50 +0100

    I#m about to change how attachSketchListeners is handled as now i cannot cange color from hard coded black

--- learning/assignments/etch-a-sketch/script.js
+++ learning/assignments/etch-a-sketch/script.js
@@ -7,8 +7,10 @@ const pad = document.querySelector(".parent");
 const checkedEl = document.querySelector(".grid__input");
 const clearBtn = document.querySelector(".js-clearGrid");
 const blackBtn = document.querySelector(".js-blackBtn");
-const sketches = document.querySelectorAll(".sketch");
-let newColor = "black";
+const eraseBtn = document.querySelector(".js-eraser");
+let sketches = document.querySelectorAll(".sketch");
+
+let newColor = "";
 let mousedown = false;
 
 document.addEventListener("mousedown", () => {
@@ -19,12 +21,6 @@ document.addEventListener("mouseup", () => {
   mousedown = false;
 });
 
-// Main hover mouse clicked logic
-sketches.forEach((sketch) => {
-  console.log("mose over");
-  sketch.addEventListener("mousemove", changeColor);
-});
-
 chageBtn.addEventListener("click", () => {
   if (gridNum.value > 100) {
     document.querySelector(".error").innerText = "Value over 100";
@@ -33,12 +29,28 @@ chageBtn.addEventListener("click", () => {
   }
 });
 
+//listener for clearing the grid
+clearBtn.addEventListener("click", clearGrid);
+//listener for erasing the cell
+eraseBtn.addEventListener("click", eraseColor);
+
 // Listener to grid tick that clears or adds grid lines
 checkedEl.addEventListener("click", () => {
   gridLines();
 });
 
+attachSketchListeners();
+// Main hover mouse clicked logic
+function attachSketchListeners() {
+  // const sketches = document.querySelectorAll(".sketch");
+  sketches.forEach((sketch) => {
+    console.log("mose over");
+    sketch.addEventListener("mousemove", blackColor);
+  });
+}
 function gridLines() {
+  // const sketches = document.querySelectorAll(".sketch");
+
   console.log("grid lines innitiated");
   console.log(checkedEl.checked);
   if (!checkedEl.checked) {
@@ -62,11 +74,36 @@ function paintGrid() {
     newDiv.className = "sketch";
     pad.appendChild(newDiv);
   }
+  sketches = document.querySelectorAll(".sketch");
+
   gridLines();
+  attachSketchListeners();
 }
 
-function changeColor() {
+//change color function for black chosen color and rainbow mode
+function blackColor() {
+  newColor = "black";
   if (mousedown) {
     this.style.backgroundColor = newColor;
   }
+}
+//change color for eraser
+function eraseColor() {
+  console.log("erase initiated");
+  newColor = "white";
+  if (mousedown) {
+    console.log("erase in if statement");
+
+    this.style.backgroundColor = newColor;
+  }
+}
+
+// function to clear the grid
+function clearGrid() {
+  console.log("clear grid initiated");
+  sketches.forEach((sketch) => {
+    console.log("clear grid  changing color to white?");
+
+    sketch.style.backgroundColor = "white";
+  });
 }
