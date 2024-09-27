c87198c6e5f391d84d4038d2f09f6716bd1080b8
Ray-Remote <przemox2@gmail.com>
2024-09-27 20:30:28 +0100

    gogo

--- practice/js Challanges/jsProjects/weightConverter/script.js
+++
@@ -1,8 +0,0 @@
-const input = document.getElementById("input");
-const result = document.getElementById("result");
-
-input.addEventListener("input", () => {
-  const inputVal = input.value;
-  const final = (inputVal / 2.2).toFixed(2);
-  return (result.innerText = final);
-});
