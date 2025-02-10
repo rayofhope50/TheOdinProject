afde643eab90fa382bd84ddc40006146248978ab
Ray-Remote <przemox2@gmail.com>
2025-02-10 14:45:17 +0000

    practice questions answered

--- practice/practice.js
+++ practice/practice.js
@@ -1,31 +1,60 @@
-const people = [
-  {
-    name: "Carly",
-    yearOfBirth: 1942,
-    yearOfDeath: 1970,
-  },
-  {
-    name: "Ray",
-    yearOfBirth: 1962,
-    yearOfDeath: 2011,
-  },
-  {
-    name: "Jane",
-    yearOfBirth: 1912,
-    yearOfDeath: 1941,
-  },
-];
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function () {
+    return `${title} by ${author}, ${pages} long,  ${read}`;
+  };
+}
 
-const findTheOldest = function (people) {
-  console.log("start");
-  const oldestPerson = people.reduce((oldest, person) => {
-    const currentPerson = person.yearOfDeath - person.yearOfBirth;
-    const oldestPerson = oldest.yearOfDeath - oldest.yearOfBirth;
-    console.log("start2");
+const theHobbit = new Book(
+  `The Hobbit`,
+  `J.R.R Tolkien`,
+  `295 pages`,
+  `not read`
+);
 
-    return currentPerson > oldestPerson ? person : oldest;
-  });
-  return oldestPerson;
+console.log(theHobbit.info());
+
+Book.prototype.sayHello = function () {
+  console.log(`hello, i'm a book.`);
 };
 
-console.log(findTheOldest(people));
+theHobbit.sayHello();
+
+let head = {
+  glasses: 1,
+};
+
+let table = {
+  pen: 3,
+  __proto__: head,
+};
+
+let bed = {
+  sheet: 1,
+  pillow: 2,
+  __proto__: table,
+};
+
+let pockets = {
+  money: 2000,
+  __proto__: bed,
+};
+
+console.log(pockets.pen);
+console.log(pockets.glasses);
+
+const iterations = 1e6;
+console.time(`Direct access (head.glasses)`);
+for (let i = 0; i < iterations; i++) {
+  let value = head.glasses;
+}
+console.timeEnd(`Direct access (head.glasses)`);
+
+console.time(`Prototype chain access (pockets.glasses)`);
+for (let i = 0; i < iterations; i++) {
+  let value = pockets.glasses;
+}
+console.timeEnd(`Prototype chain access (pockets.glasses)`);
